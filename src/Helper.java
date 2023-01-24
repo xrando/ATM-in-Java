7213be47b94937fd68cf0699e1249929b7293852
Benjamin Loh <xrando20@gmail.com>
2023-01-24 17:10:04 +0800

    added TalkToSpeech function and external library

--- src/Helper.java
+++ src/Helper.java
@@ -8,6 +8,11 @@ import java.util.*;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+
+import java.util.Locale;
+import javax.speech.Central;
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
 public class Helper
 {
     public static void WriteDoc(String content)
@@ -99,6 +104,33 @@ public class Helper
         catch (Exception e)
         {
             System.out.printf("Error due to : %s",e.getCause());
+            e.printStackTrace();
+        }
+    }
+    //sample
+    //TextToSpeech.TTS("Hi my name is benjamin");
+    public static void TTS(String Text)
+    {
+        try
+        {
+            //setting properties as Kevin Dictionary
+            System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us" + ".cmu_us_kal.KevinVoiceDirectory");
+            //registering speech engine
+            Central.registerEngineCentral("com.sun.speech.freetts" + ".jsapi.FreeTTSEngineCentral");
+            //create a Synthesizer that generates voice
+            Synthesizer synthesizer = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
+            //allocates a synthesizer
+            synthesizer.allocate();
+            //resume a Synthesizer
+            synthesizer.resume();
+            //speak the specified text until the QUEUE become empty
+            synthesizer.speakPlainText(Text, null);
+            synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
+            //deallocating the Synthesizer
+            synthesizer.deallocate();
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
     }
