55965128f00c1f674d280cfa60cb7b0697f2dc2b
L4ncelot-R <wrongqir@icloud.com>
2023-03-11 00:04:20 +0800

    added abstract class for ssl

--- src/ATM/Client/Client.java
+++ src/ATM/Client/Client.java
@@ -1,12 +1,20 @@
 package ATM.Client;
 
 import ATM.Constants.Constants;
+import ATM.Utilities.LogHelper;
+
+import java.io.IOException;
+import java.util.logging.Level;
 
 public class Client {
-    private final ATMClientSocket socket;
+    private ATMClientSocket socket;
 
     public Client() {
-        socket = new ATMClientSocket(); //start new instance of client socket
+        try {
+            socket = new ATMClientSocket(); //start new instance of client socket
+        } catch (IOException e) {
+            LogHelper.log(Level.SEVERE, "Server is not ready, or wrong host IP / port number.", e);
+        }
     }
 
     public String listen(String input) {
